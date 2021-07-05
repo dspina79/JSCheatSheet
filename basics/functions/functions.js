c5069b53437e360c7bb35c6ee12eeafc454fa622
Dave Spina <dspina79@gmail.com>
2021-07-05 18:24:06 -0400

    Added more basic function handling

--- basics/functions/functions.js
+++ basics/functions/functions.js
@@ -13,3 +13,31 @@ function add(x, y) {
 }
 
 add(1, 2);
+
+// function score
+var myGlobalVar = 10; // global variable
+
+function testVariableScope() {
+    var myLocalVar = 100;
+    myGlobalVar = 20;
+
+    console.log(myLocalVar);
+}
+
+testVariableScope();
+
+// myLocalVar is not defined
+ console.log("myGlobalVar = " + myGlobalVar); // 20
+
+
+ // variables and returns
+ function addToFixedQueue(arr, item) {
+    arr.push(item);
+    arr.shift();
+    return arr.length;
+ }
+
+ var myFixedQueue = [1,2,3,4,5,6];
+ var myQueueLength = addToFixedQueue(myFixedQueue, 7);
+console.log(myFixedQueue); // [2,3,4,5,6,7]
+console.log(myQueueLength); // 6
