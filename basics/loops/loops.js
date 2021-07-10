de046feeae385d6803bcab7233e5b09c9359fcec
Dave Spina <dspina79@gmail.com>
2021-07-09 21:29:50 -0400

    Adding loops

---
+++ basics/loops/loops.js
@@ -0,0 +1,47 @@
+// basic while loop
+var x = 5;
+while (x > 0) {
+    console.log(x);
+    x--;
+} // should print 5, 4, 3, 2, 1
+
+// basic do...while loops
+// will run at least once
+var y = 10;
+do {
+    console.log(y);
+    y--;
+} while (y > 5)
+
+// this will still run once
+y = 10;
+do {
+    console.log(y);
+    y--;
+} while (y > 10)
+
+
+// for loops
+for (var i = 0; i < 10; i++) {
+    console.log('Printing...' + i);
+}
+
+for (var i = 0; i < 10; i+=2) {
+    console.log('Primarily printing even numbers...' + i);
+}
+console.log('Zero doesn\'t count');
+
+/*
+    RECURSION
+*/
+
+// using recursion for factorials
+function factorial(x) {
+    if (x === 1) {
+        return 1;
+    } 
+    
+    return x * factorial(x - 1);
+}
+
+console.log("5! = " + factorial(5));
