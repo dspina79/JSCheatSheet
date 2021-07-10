ca9be3a20051435b8e2926c3ded8dd6fc8b39fa5
Dave Spina <dspina79@gmail.com>
2021-07-10 09:53:42 -0400

    Added mention of parseInt

--- basics/misc/specialfunctionsandops.js
+++ basics/misc/specialfunctionsandops.js
@@ -20,3 +20,20 @@ for (var i = 0; i < equalityTesters.length; i++) {
     var result = checkEquality(xElement, yElement);
     console.log("Are " + xElement + " and " + yElement + " are " + result);
 }
+
+// Special Functions
+
+// parseInt - parses values to integers
+var myStringValue = "15";
+var myBinaryStringValue = "10100101";
+
+console.log(parseInt(myStringValue));
+console.log(parseInt(myBinaryStringValue, 2)); // indicates base 2
+
+var myStringValues = [["124", 10],["101010111", 2],["0xFF", 16],["71", 8]];
+for (var i = 0; i < myStringValues.length; i++) {
+    var val = myStringValues[i][0];
+    var b = myStringValues[i][1];
+    var result = parseInt(val, b); // value and base
+    console.log("The base 10 int value of " + val + " base(" + b + ") is " + result);
+}
