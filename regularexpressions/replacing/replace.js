f4f0906f742f208a84bcf1600fdc4109b310254f
Dave Spina <dspina79@gmail.com>
2021-07-18 21:53:12 -0400

    New examples of replacement

--- regularexpressions/replacing/replace.js
+++ regularexpressions/replacing/replace.js
@@ -4,5 +4,12 @@
 
 const helloRegex = /hello/ig;
 const sentence = "The art of saying hello without saying 'hello'";
-const newSentence = sentence.replace(helloRegex, "Goodbye");
+const newSentence = sentence.replace(helloRegex, "Goodbye"); 
 console.log(newSentence);
+// ^^^ outputs The art of saying Goodbye without saying 'Goodbye'
+
+// replace all whitespace with underscores
+const spaceRegex = /\s/g;
+const underscoreSentence = sentence.replace(spaceRegex, "_");
+console.log(underscoreSentence); 
+// ^^^ outputs The_art_of_saying_hello_without_saying_'hello'
