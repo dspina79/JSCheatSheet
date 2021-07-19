76e6e9bb7f4a05ba43e855b8ea1c49c73dae5b58
Dave Spina <dspina79@gmail.com>
2021-07-18 21:18:51 -0400

    Regex lookaheads

--- regularexpressions/matching/repeatingcounts.js
+++ regularexpressions/matching/repeatingcounts.js
@@ -54,3 +54,26 @@ Woooooord
 /Wo{3,}rd/ = true
 /Wo{4}rd/ = false
 */
+
+// Look Aheads with ?= and ?!
+// ?= match the pattern somewhere in the code
+// ?! do not match the pattern anywhere in the pattern
+
+const containsThreeWhitespaces = /(?=\s{3})/; //contains at least three consecutive white spaces
+const doesNotContainThreeWhitespaces = /(?!\s{3})/; // does not match any instance of 3 whitespaces
+const testWhitespacePattern1 = "This is a pattern with   a couple of spaces.";
+const testWhitespacePattern2 = "This one has single spaces";
+
+console.log(containsThreeWhitespaces.test(testWhitespacePattern1)); // outputs true
+console.log(containsThreeWhitespaces.test(testWhitespacePattern2)); // outputs false
+
+console.log(doesNotContainThreeWhitespaces.test(testWhitespacePattern1)); 
+// ^^ outputs true because there are instances of non-three-repeating whitespace characters
+console.log(doesNotContainThreeWhitespaces.test(testWhitespacePattern2)); // outputs true
+
+// Example of complex lookaheads
+const simpleUsernameCheck = /\w+@(?=\D{3}\d{2})/; // username must be some text followed by @ then 3 nondigits followed by 2 digits
+const usernameChecks = ["brian@som33", "david.wilkes@oldie", "a8s9f79.sjf22", "laura@hp222"];
+for (var i = 0; i < usernameChecks.length; i++) {
+    console.log(`${usernameChecks[i]}: ${simpleUsernameCheck.test(usernameChecks[i])}`);
+}
