6413379e3ec62198b2e647ef6212e8b301f19c8f
Dave Spina <dspina79@gmail.com>
2021-07-24 08:53:43 -0400

    Property handling in OOP

--- oop/basics.js
+++ oop/basics.js
@@ -31,3 +31,55 @@ const oddNumber = 13;
 console.log(cat instanceof Animal); // outputs true
 console.log(dog instanceof Animal); // outputs true
 console.log(oddNumber instanceof Animal); // outputs false
+
+// checking for properties using hasOwnProperty
+// if a variable does not have the property, the method returns false
+const testProperties = ["name", "age", "weight"];
+for (let prop of testProperties) {
+    console.log(`Testing property '${prop}'`);
+    if (cat.hasOwnProperty(prop)) {
+        console.log('The cat has that property');
+    } else {
+        console.log('The cat does not have the property.');
+    }
+}
+
+/*
+Outputs
+=======
+Testing property 'name'
+The cat has that property
+Testing property 'age'
+The cat has that property
+Testing property 'weight'
+The cat does not have the property.
+*/
+
+// Adding Properties to Instances
+// simply adding a new property to an **instance** just
+// requires simple dot notation
+
+cat.furColor = 'black';
+console.log(`The cat, ${cat.name}, has ${cat.furColor} fur.`);
+// ^^^ outputs "The cat, Sebastian, has black fur.""
+console.log(cat.hasOwnProperty('furColor')); // outputs true
+console.log(dog.hasOwnProperty('furCOlor')); // outputs false
+
+// Iterating Over Properties
+// using for..in, each property can be isolated
+
+for (let prop in cat) {
+    if (cat.hasOwnProperty(prop)) {
+        console.log(`cat.${prop} is present.`)
+    } else {
+        console.log(`cat.${prop} is not present.`)
+    }
+}
+
+/*
+Outputs
+=======
+cat.name is present.
+cat.age is present.
+cat.furColor is present.
+*/
