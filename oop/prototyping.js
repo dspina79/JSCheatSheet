22af58f8936034e4341544b2a105641fe9919417
Dave Spina <dspina79@gmail.com>
2021-07-24 16:02:21 -0400

    Added section on constructors in prototypes

--- oop/prototyping.js
+++ oop/prototyping.js
@@ -40,3 +40,55 @@ cube HAS the height property.
 cube HAS the width property.
 cube HAS the depth property.
 */
+
+
+// Creating New Objects with Prototype
+// starting with a constructor
+function Person(firstName, lastName, email) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.emailAddress = email;
+}
+
+Person.prototype = {
+    age: 0,
+    greet: function() {
+        return `Hello, my name is ${this.firstName}`;
+    },
+    birthday: function() {
+        this.age++;
+    }
+}
+
+let jacob = new Person("Jacob", "Smoke", "jake@lostnowhere.net");
+jacob.age = 230;
+console.log(jacob.greet());
+jacob.birthday();
+console.log(`${jacob.firstName} ${jacob.lastName} is ${jacob.age} years old.`);
+
+/*
+Outputs
+=======
+Hello, my name is Jacob
+Jacob Smoke is 231 years old.
+*/
+
+// Constructor Desctruction
+// when the prototype is set, the constructor can be destroyed. 
+// therefore, when generating the new prototype, set the constructor.
+
+function Book(title, author) {
+    this.title = title;
+    this.author = author;
+}
+
+Book.prototype = {
+    constructor: Book,
+    numberPages: 0,
+    read: function() {
+        console.log(`Currently reading ${this.title} by ${this.author}`);
+    }
+}
+
+let taleOfTwoCities = new Book("A Tale of Two Cities", "Charles Dickens");
+console.log(taleOfTwoCities.constructor === Book); //outputs true
