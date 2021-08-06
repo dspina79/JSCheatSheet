bacc79a10b950b34a60750c9bac0acaec36ee7c6
Dave Spina <dspina79@gmail.com>
2021-08-06 15:22:03 -0400

    Wrote new method for writing to a new file

--- smallprojects/simpleio/simpleio.js
+++ smallprojects/simpleio/simpleio.js
@@ -11,8 +11,18 @@ function outputFile(filePath) {
     });
 }
 
+function writeNewFile(filePath, starterText) {
+    fs.writeFile(filePath, starterText, "utf8", function(error, data) {
+        if (data) {
+            console.log(`Successfully wrote file: ${data}.`);
+        } else if (error) {
+            console.log(`An error occurred when starting the file: ${error}.`);
+        }
+    });
+}
+
 function appendFile(filePath, newText) {
-    fs.appendFile(filePath, newText, "utf8", function(error, data) {
+    fs.appendFile(filePath, `\n${newText}`, "utf8", function(error, data) {
         if (data) {
             console.log("Wrote data");
         } else if (error) {
@@ -24,3 +34,9 @@ function appendFile(filePath, newText) {
 outputFile("files/simpleread.txt");
 appendFile("files/simpleread.txt", "a brave new world");
 outputFile("files/simpleread.txt");
+
+writeNewFile("files/studyexcerpt.txt", "Do you remember what Darwin says about music? He claims that the power of producing and appreciating it existed among the human race long before the power of speech was arrived at.");
+appendFile("files/studyexcerpt.txt", "Perhaps that is why we are so subtly influenced by it."); 
+appendFile("files/studyexcerpt.txt", "There are vague memories in our souls of those misty centuries when the world was in its childhood.")
+
+outputFile("files/studyexcerpt.txt");
